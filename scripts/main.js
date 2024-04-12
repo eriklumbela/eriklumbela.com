72e74b5353ae8428cf52b9c4764d8c05936c5591
Erik Lumbela <erik@lumbela.nl>
2024-04-12 12:11:43 +0200

    Add colored circle favicon generation functionality

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,30 @@ function externalLinks() {
 }
 
 externalLinks()
+
+function generateFavicon(color) {
+  const canvas = document.createElement("canvas");
+  canvas.width = canvas.height = 16;
+  const context = canvas.getContext("2d");
+  context.fillStyle = color;
+
+  context.beginPath();
+  context.arc(8, 8, 8, 0, 2 * Math.PI);
+  context.fill();
+
+  const imageUrl = canvas.toDataURL();
+  
+  const link = document.createElement("link");
+  link.setAttribute("rel", "icon");
+  document.querySelector("head").appendChild(link);
+
+  link.type = "image/x-icon";
+  link.href=imageUrl;
+};
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+
+generateFavicon(getRandomColor())
